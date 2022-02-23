bcfd6ad99751f65f83e25ab532c250695b00e142
devendra-bari <98512378+devendra-bari@users.noreply.github.com>
2022-02-23 19:22:03 +0530

    Update RemoveElementFromArrayList.java

--- RemoveElementFromArrayList.java
+++ RemoveElementFromArrayList.java
@@ -9,14 +9,14 @@ public class RemoveElementFromArrayList
       String s1 = "";
       Scanner s = new Scanner(System.in);
 
-        System.out.println("\tREMOVING ELEMENT From ARRAYLIST DEMO\n");
+      System.out.println("\tREMOVING ELEMENT From ARRAYLIST DEMO\n");
 
-        System.out.print(" Enter Size of List\t:\t");
-        a = s.nextInt();
+      System.out.print(" Enter Size of List\t:\t");
+      a = s.nextInt();
 
-        ArrayList<String> arr = new ArrayList<String>(a);
+      ArrayList<String> arr = new ArrayList<String>(a);
 
-        System.out.println("\n Enter the Elements...\n");
+      System.out.println("\n Enter the Elements...\n");
       s.nextLine();
 
       for(int i=0; i < a; i++)
@@ -26,29 +26,29 @@ public class RemoveElementFromArrayList
           arr.add(s1);
       }
 
-        System.out.println("\n The size of the list is :\t" + arr.size());
+      System.out.println("\n The size of the list is :\t" + arr.size());
 
       int i=0;
-        for (String name : arr)
-        {
-           System.out.print("\n " + (i+1) + ". "+ name);
-           i++;
-        }
+      for (String name : arr)
+      {
+         System.out.print("\n " + (i+1) + ". "+ name);
+         i++;
+      }
 
       System.out.print("\n\n Enter no. of element to be removed\t:\t");
 
-        a = s.nextInt();
-        arr.remove(a-1);
+      a = s.nextInt();
+      arr.remove(a-1);
 
-        System.out.println("\n After element removed, size of List\t:\t" + arr.size());
-        System.out.println("\n And the Altered List is...");
+      System.out.println("\n After element removed, size of List\t:\t" + arr.size());
+      System.out.println("\n And the Altered List is...");
 
       i=0;
-        for (String name : arr)
-        {
-           System.out.print("\n " + (i+1) + ". " + name);
-           i++;
-        }
-        System.out.print("\n\n\n\n\n ");
-     }
+      for (String name : arr)
+      {
+         System.out.print("\n " + (i+1) + ". " + name);
+         i++;
+      }
+      System.out.print("\n\n\n\n\n ");
+   }
 }
